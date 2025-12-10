e62bcab0a23012ce6d023bea3c9b4176e9fed691
nathanbc46 <nathanbc46@gmail.com>
2025-12-10 15:28:13 +0700

    feat: scaffold initial Vue 3 + TypeScript + Vite project with CI workflow

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/test-ci/', // 👈 ต้องระบุ
+})
